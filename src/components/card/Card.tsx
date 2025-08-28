6b4d18cdec6bc5cd4e42cf163982882b315428d2
Alien501 <cvignesh404@gmail.com>
2025-08-28 15:50:24 +0530

    Dummy animations

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -9,7 +9,7 @@ interface CardProps {
 
 export default function Card({title, child, icon=<PersonSpeakIcon />}: CardProps) {
     return(
-        <div className="relative h-full perspective-1000 overflow-hidden card outline outline-primary-light/10">
+        <div className="card-container relative h-full perspective-1000 overflow-hidden card outline outline-primary-light/10">
             <div className="h-full bg-primary-dark/2 p-2 backdrop-blur-2xl hover:outline hover:outline-primary-light/10 hover:box-shadow
                             hover:bg-gradient-to-b hover:from-[var(--border-from-color)]
                             hover:to-[var(--border-to-color)] hover:bg-clip-content-box
@@ -20,7 +20,7 @@ export default function Card({title, child, icon=<PersonSpeakIcon />}: CardProps
                 <div className="relative z-10">
                     <div className="card-title-wrapper flex justify-center -space-x-[0.5] -top-2 items-center">
                         <span>{icon}</span>
-                        <p className="font-aeonik-bold text-xl text-gradient text-shadow text-center">{title}</p>
+                        <p className="font-aeonik-bold text-xl text-gradient text-shadow text-center relative z-10 card-heading-text overflow-hidden p-[0.5]">{title}</p>
                     </div>
                     <div className="h-full mt-4 card-child-container font-wotfard text-xs tracking-wide leading-4 text-primary-dark/80 overflow-y-scroll thin-scrollbar">
                         {child}
--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -1,11 +1,58 @@
+"use client";
+
+import gsap from 'gsap';
+import './HeroSection.css';
+import { useEffect, useRef } from 'react';
+import { SplitText } from 'gsap/SplitText'
+
+gsap.registerPlugin(SplitText);
+
 export default function HeroSection() {
+    const heroWrapperRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        if(heroWrapperRef.current) {
+            const heroFakeName = document.querySelector('.hero-fake-name');
+            const heroRealName = document.querySelector('.hero-real-name');
+
+            const heroAnimation = gsap.timeline({paused: true})
+                                    .to(heroFakeName, {
+                                        rotateX: 90,
+                                        duration: .3,
+                                        transformOrigin: 'top',
+                                    }).fromTo(heroRealName, {
+                                        rotateX: -90,
+                                        opacity: 0,
+                                    }, {
+                                        rotateX: 0,
+                                        transformOrigin: 'bottom',
+                                        opacity: 1,
+                                    }, '<')
+
+            heroWrapperRef.current.addEventListener('mouseenter', () => {
+                heroAnimation.play();
+            })
+            heroWrapperRef.current.addEventListener('mouseleave', () => {
+                heroAnimation.reverse();
+            })
+        }
+    }, []);
+
     return(
         <div className="h-full flex justify-center items-center text-center">
-            <div>
-                <p className="font-aeonik-bold lg:text-8xl text-4xl bg-gradient-to-t from-primary-light to-primary-dark bg-clip-text text-transparent text-shadow">ALIEN501</p>
-                <p className="font-wotfard text-xl tracking-wide bg-gradient-to-t from-primary-light to-primary-dark bg-clip-text text-transparent text-shadow">Software Developer</p>
+            <div ref={heroWrapperRef} className="bg-white/0 relative overflow-hidden hero-text-wrapper">
+                <div className='hero-fake-name'>
+                    <p className="font-aeonik-bold lg:text-8xl text-4xl bg-gradient-to-t from-primary-light to-primary-dark bg-clip-text text-transparent text-shadow z-10">
+                        ALIEN501
+                    </p>
+                </div>
+                <div className='absolute -top-[0px] hero-real-name text-center bg-white/0 mx-auto w-full'>
+                    <p className="font-aeonik-bold lg:text-8xl text-4xl bg-gradient-to-t from-primary-light to-primary-dark bg-clip-text text-transparent text-shadow z-10">
+                        VIGNESH
+                    </p>
+                </div>
+                <p className="font-doto font-semibold text-xl tracking-wide bg-gradient-to-t from-primary-light to-primary-dark bg-clip-text text-transparent text-shadow">SOFTWARE DEVELOPER</p>
             </div>
-            {/* <img src="/border.svg" alt="" className="text-white bg-white" /> */}
         </div>
     )
 }
